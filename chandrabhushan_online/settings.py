bb97f7f34526530d2e8a5eced6ab6d765b8c8234
Amlesh <amleshkumar647544@gmail.com>
2026-02-21 18:13:12 +0530

    final commit

--- chandrabhushan_online/settings.py
+++ chandrabhushan_online/settings.py
@@ -8,9 +8,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-change-this-in-production-chandrabhushan-online-2024'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['chandrabhushan-online.onrender.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
