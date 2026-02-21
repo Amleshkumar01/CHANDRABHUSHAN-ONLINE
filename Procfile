6911365a7980afe2944de165d384b20a3ce0ac42
Amlesh <amleshkumar647544@gmail.com>
2026-02-21 15:44:38 +0530

    ready for deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn chandrabhushan_online.wsgi
--- chandrabhushan_online/settings.py
+++ chandrabhushan_online/settings.py
@@ -76,6 +76,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static'] if (BASE_DIR / 'static').exists() else []
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Media files (user uploads - product images)
 MEDIA_URL = 'media/'
